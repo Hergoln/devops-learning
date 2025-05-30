047e5799f52e853858296fcf2f5645ed02ac8435
Hergoln <juliusz.malka@o2.pl>
2025-05-30 21:09:19 +0200

    split code into files

--- go/main.go
+++ go/main.go
@@ -17,22 +17,6 @@ var (
 	BYTE_READ_ERROR int = 4
 )
 
-type Repository struct {
-	Name string
-	Url string
-	ContentsUrl string
-}
-
-type GHContent struct {
-	Name 					string `json:"name"`
-	Path 					string `json:"path"`
-	Url 					string `json:"url"`
-	Git_url 			string `json:"git_url"`
-	Download_url 	string `json:"download_url"`
-	Type 					string `json:"type"`
-	Content				string `json:"content"`
-}
-
 /* task #9
 	1. gather all repositories token has access to
 	2. use https://docs.github.com/en/rest/repos/contents?apiVersion=2022-11-28#get-repository-content api endpoint to get directory .github/workflows
@@ -54,32 +38,6 @@ func main() {
 	callGitHubAPI(PAT)
 }
 
-func callAPI(url string, headers map[string]string) *http.Response {
-	fmt.Println("Calling url: ", url)
-	req, err := http.NewRequest(http.MethodGet, url, nil)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(REQUEST_ERROR)
-	}
-
-	for header, value := range headers {
-		req.Header.Set(header, value)
-	}
-
-	client := http.Client{
-		Timeout: 30 * time.Second,
-	}
-
-	res, err := client.Do(req)
-
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(API_CALL_ERROR)
-	}
-
-	return res
-}
-
 func callGitHubAPI(PAT string) {
   requestURL := "https://api.github.com/user/repos"
 	headers := map[string]string{
@@ -97,34 +55,6 @@ func callGitHubAPI(PAT string) {
 	}
 }
 
-func unmarshalArray(body []byte) []map[string]any {
-	var jsonBody []map[string]any
-
-	err := json.Unmarshal(body, &jsonBody)
-	if err != nil {
-		fmt.Println(err.Error())
-		os.Exit(UNMARSHAL_ERROR)
-	}
-
-	return jsonBody
-}
-
-func readBody(response *http.Response) []byte {
-	defer response.Body.Close()
-	bytes, err := ioutil.ReadAll(response.Body)
-	
-	if response.StatusCode == http.StatusOK {	
-		if err != nil {
-			fmt.Println(err.Error())
-			os.Exit(BYTE_READ_ERROR)
-		}
-		return bytes
-	}
-
-	fmt.Println("No body found")
-	return nil
-}
-
 func extractRepos(jsonBody []map[string]any) []*Repository {
 	reposCount := len(jsonBody)
 	output := make([]*Repository, 0, reposCount)
@@ -134,15 +64,6 @@ func extractRepos(jsonBody []map[string]any) []*Repository {
 	}
 
 	return output
-}
-
-func newRepository(jsonRepo map[string]any) *Repository {
-	repo := Repository{
-		Name: jsonRepo["full_name"].(string),
-		Url: jsonRepo["url"].(string),
-		ContentsUrl: jsonRepo["contents_url"].(string),
-	}
-	return &repo
 }
 
 func gatherWorkflows(repo *Repository, headers map[string]string) []map[string]any {
@@ -172,20 +93,4 @@ func extractWorkflows(jsonBody []map[string]any) []*GHContent {
 	}
 
 	return output
-}
-
-func newContent(jsonContent map[string]any) *GHContent {
-  workflow := GHContent{
-		Name: 				jsonContent["name"].(string),
-		Path: 				jsonContent["path"].(string),
-		Url: 					jsonContent["url"].(string),
-		Git_url: 			jsonContent["git_url"].(string),
-		Download_url: jsonContent["download_url"].(string),
-		Type: 				jsonContent["type"].(string),
-	}
-	// if workflow.Type == "file" {
-	// 	fmt.Println(jsonContent["content"])
-	// 	workflow.Content = jsonContent["content"].(string)
-	// }
-	return &workflow
 }
---
+++ go/stdAPIOps.go
@@ -0,0 +1,27 @@
+package main
+
+func callAPI(url string, headers map[string]string) *http.Response {
+	fmt.Println("Calling url: ", url)
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(REQUEST_ERROR)
+	}
+
+	for header, value := range headers {
+		req.Header.Set(header, value)
+	}
+
+	client := http.Client{
+		Timeout: 30 * time.Second,
+	}
+
+	res, err := client.Do(req)
+
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(API_CALL_ERROR)
+	}
+
+	return res
+}
