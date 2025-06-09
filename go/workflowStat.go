6dd9a3a64dfb189cbe7f9630950e6ed140468ca5
Hergoln <juliusz.malka@o2.pl>
2025-06-09 21:52:55 +0200

    added toCSVRows function

--- go/workflowStat.go
+++ go/workflowStat.go
@@ -1,15 +1,36 @@
 package main
 
+import (
+	"fmt"
+	"strings"
+)
+
 type WorkflowStat struct {
 	Path 				string
 	RepoUrl 		string
-	References  []*Reference
+	Usings  []*Usage
 }
 
-type Reference struct {
-	RefRepo string
-	Ref string
+type Usage struct {
+	// whatever comes after "uses:"
+	UsesPath string
+	// Action, Workflow (?)
+	Type string
 }
 
-// func toCSV()
+func newWorkflowStat(path string, repoUrl string) *WorkflowStat {
+	return &WorkflowStat{
+		Path: path,
+		RepoUrl: repoUrl,
+	}
+}
+
 // func newWorkflowStat()
+func (stat *WorkflowStat) toCSVRows() []string {
+	base := make([]string, 0, len(stat.Usings))
+	for idx := range stat.Usings {
+		row := strings.Join([]string{stat.Path, stat.RepoUrl, stat.Usings[idx].UsesPath, stat.Usings[idx].Type}, ",")
+		base = append(base, row)
+	}
+	return base
+}
